d1801041dcc8f1dac704672b1bc0b68d40b43648
Pradikshan <pradikshan234@gmail.com>
2024-09-26 08:56:57 +0530

    Added title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: "Home - Joe's Robot Shop" },
+];
 
 @NgModule({
   declarations: [],
